bc73a1567df4db5d027387a3b9b2cdf83bcc3770
Samrajya Pratap Rana <phinix0980@gmail.com>
2026-05-01 10:08:16 +0545

    updated test client

--- src/app/(dashboard)/tests/client/tests-client.ts
+++ src/app/(dashboard)/tests/client/tests-client.ts
@@ -1,7 +1,14 @@
 import { clientHttp } from '@/lib/axios/client.axios';
 import { Test } from '@/types/tests';
 import { Meta } from '@/types/data-response-meta';
-import { TestFormValues, TestUpdateFormValues } from '../types/test-form.types';
+import {
+  ReferenceRangeValues,
+  ResultValueOptionValues,
+  TestFormValues,
+  TestUpdateFormValues,
+} from '../types/test-form.types';
+import { ReferenceRange } from '@/types/reference-range';
+import { ResultValueOption } from '@/types/result-value-option';
 
 class TestClient {
   private readonly TESTS_ENDPOINT = '/tests';
@@ -28,7 +35,48 @@ class TestClient {
   async updateTest(id: number, data: TestUpdateFormValues) {
     return await clientHttp.patch<Test>(`${this.TESTS_ENDPOINT}/${id}`, data);
   }
-}
 
+  async addRefRangesForTest(testId: number, data: ReferenceRangeValues) {
+    return await clientHttp.post<ReferenceRange>(
+      `${this.TESTS_ENDPOINT}/${testId}/reference-ranges`,
+      data
+    );
+  }
+
+  async updateRefRangeForTest(refRangeId: number, data: ReferenceRangeValues) {
+    return await clientHttp.patch<ReferenceRange>(
+      `${this.TESTS_ENDPOINT}/reference-ranges/${refRangeId}`,
+      data
+    );
+  }
+  async deleteRefRangeForTest(refRangeId: number) {
+    return await clientHttp.delete<true>(
+      `${this.TESTS_ENDPOINT}/reference-ranges/${refRangeId}`
+    );
+  }
+
+  async addResultOptionsForTest(testId: number, data: ResultValueOptionValues) {
+    return await clientHttp.post<ResultValueOption>(
+      `${this.TESTS_ENDPOINT}/${testId}/result-value-options`,
+      data
+    );
+  }
+
+  async updateResultOptionsForTest(
+    testId: number,
+    data: ResultValueOptionValues
+  ) {
+    return await clientHttp.patch<ResultValueOption>(
+      `${this.TESTS_ENDPOINT}/${testId}/result-value-options`,
+      data
+    );
+  }
+
+  async deleteResultOptionsForTest(resultValueOptionId: number) {
+    return await clientHttp.delete<true>(
+      `${this.TESTS_ENDPOINT}/result-value-options/${resultValueOptionId}`
+    );
+  }
+}
 const testClient = new TestClient();
 export default testClient;
